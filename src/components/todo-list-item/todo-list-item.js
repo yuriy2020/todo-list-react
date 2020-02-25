42d70810d7116e43dbeeb979dce90e45fcb2ec6a
Yuri Balalaev <yuriy8888@yandex.ru>
2020-02-25 16:57:43 +0300

    React & Redux. 021 State состояние React компонента -зачеркивает список

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,17 +4,53 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends React.Component {
 
+  constructor() {
+    super();
+
+    // после инициализации state его напрямую изменять нельзя 
+    this.state = {
+      done: false
+    };
+
+    this.onLableClick = () => {
+      // для state можно использовать специальную ф-цию this.setState
+      // и в эту функцию мы можем внести объект с изменениями кооторые хотим внести в state
+      this.setState({
+        done: true
+      })
+    }
+
+  }
+
+  //ф-ция на клик по списку на прототипе класса
+  // onLableClick() {
+  //   console.log('Done', this.props.label)
+  // }
+
   render() {  //отображает туже функцию TodoListItemFunc - скопировал с тела функции
     // одно отличие входные параметры props теперь в this.props
     const { label, important = false } = this.props;
+
+    const { done } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done'
+    }
 
     const liStyle = {
       color: important ? 'tomato' : 'black',
       fontWeight: important ? 'bold' : 'normal'
     };
+
+
     return (
-      <span className="todo-list-item"
-        style={liStyle}>{label}
+      <span className={classNames}
+        style={liStyle}
+        onClick={this.onLableClick   // Добавление св-во  onClick (onBlur, onMouseOver...)
+          // .bind(this)
+        }
+      > {label}
 
         <button type="button"
           className="btn btn-outline-success btn-sm float-right">
